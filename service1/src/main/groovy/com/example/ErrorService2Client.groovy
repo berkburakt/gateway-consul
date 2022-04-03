f5a067b8d8460028fce5dbc75bace341f2f85f6e
Berk Burak Tasdemir <burakt546@live.com>
2022-04-03 02:24:37 -0700

    Fix

--- service1/src/main/groovy/com/example/ErrorService2Client.groovy
+++ service1/src/main/groovy/com/example/ErrorService2Client.groovy
@@ -8,7 +8,6 @@ import io.reactivex.Flowable
 class ErrorService2Client implements Service2Client {
     @Override
     Flowable<ArrayList<Book>> findBooks() {
-        Book book1 = new Book(title: "Title1", author: "author1", isbn: "xxxxxxx")
-        return Flowable.just([book1])
+        Flowable.just([])
     }
 }
--- service1/src/main/groovy/com/example/Service2Client.groovy
+++ service1/src/main/groovy/com/example/Service2Client.groovy
@@ -7,7 +7,7 @@ import io.micronaut.retry.annotation.CircuitBreaker
 import io.reactivex.Flowable
 
 @Client(id = "service2")
-@CircuitBreaker(delay = "3s", attempts = "1", multiplier = "1", reset = "20s")
+@CircuitBreaker(delay = "3s", attempts = "1", multiplier = "1", reset = "15s")
 interface Service2Client {
     @Get("/books")
     Flowable<ArrayList<Book>> findBooks();
